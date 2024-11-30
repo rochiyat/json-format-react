d6572c98699cdb2c7b9fe1ef7115107e6b1a817a
Rochiyat <rochiyat@gmail.com>
2024-11-30 15:44:46 +0700

    Update favicon and refactor JsonFormatter component
    - Replaced the existing favicon with a new one and added a backup favicon file.
    - Removed the CopyToClipboard functionality from the JsonFormatter component to simplify the UI.
    - Streamlined the action buttons in the JsonFormatter for better usability.

--- src/pages/jsonFormatter.jsx
+++ src/pages/jsonFormatter.jsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import JSONPretty from 'react-json-pretty';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import '../css/pretty-bg-white.css';
 
 export default function JsonFormatter() {
@@ -35,12 +34,6 @@ export default function JsonFormatter() {
     }
   }
 
-  const copyToClipboardHandle = async () => {
-    <CopyToClipboard text={output} onCopy={(output, result) => console.log(result)}>
-      <button>Copy</button>
-    </CopyToClipboard>
-  }
-
   return (
     <div className="max-w-2xl mx-auto p-4 space-y-4">
       <h1 className="text-2xl font-bold text-center">JSON Formatter</h1>
@@ -57,16 +50,9 @@ export default function JsonFormatter() {
         </div>
         <div className='column'>
           <h1>Action</h1>
-          <div className='size-10'>
-            <button onClick={handleFormat} className="w-full p-2 border rounded border-radius-5">
-              Format JSON
-            </button>
-          </div>
-          <div className='size-10'>
-            <CopyToClipboard text={output} onCopy={(text, result) => console.log(text)}>
-              <button>Copy JSON</button>
-            </CopyToClipboard>
-          </div>
+          <button onClick={handleFormat} className="w-full p-2 border rounded border-radius-5">
+            Format JSON
+          </button>
         </div>
         <div className='column'>
           <h1>Output JSON</h1>
